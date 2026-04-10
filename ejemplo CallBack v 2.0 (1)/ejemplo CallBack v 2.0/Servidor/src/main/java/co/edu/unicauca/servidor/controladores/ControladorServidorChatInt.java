b02944feace87cfb5f0a9aa4dad80d45b406930a
SofiaMorenoT <sofiamoreno@unicauca.edu.com>
2026-04-10 15:17:31 -0500

    Subo Version del Chat grupal #1.0

---
+++ ejemplo CallBack v 2.0 (1)/ejemplo CallBack v 2.0/Servidor/src/main/java/co/edu/unicauca/servidor/controladores/ControladorServidorChatInt.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+package co.edu.unicauca.servidor.controladores;
+
+import co.edu.unicauca.cliente.controladores.UsuarioCllbckInt;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ControladorServidorChatInt extends Remote
+{
+    public boolean registrarReferenciaUsuario(UsuarioCllbckInt  usuario) throws RemoteException;
+    public void desconectarUsuario(String nickname)
+    public void enviarMensaje(String mensaje)throws RemoteException;
+    public List <String> obtenerUsuarios() throws RemoteException;
+}
+
+
